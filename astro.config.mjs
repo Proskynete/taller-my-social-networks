208fbadc6869313ae112e72443ddb533f8bf981d
Eduardo Álvarez <soy@eduardoalvarez.dev>
2025-05-23 14:16:50 -0400

    perf: improve astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+const isProd = import.meta.env.PROD;
+
 // https://astro.build/config
 export default defineConfig({
+  site: isProd ? "https://Proskynete.github.io" : undefined,
+  base: isProd ? "taller-my-social-networks" : undefined,
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://Proskynete.github.io",
-  base: "taller-my-social-networks",
 });
